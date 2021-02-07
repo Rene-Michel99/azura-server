46e83adb0e24cfca917c2cdd79bb6848ac263e7e
Rene Michel <renemichel856@gmail.com>
2021-02-07 15:47:28 -0300

    Atualizacao

--- index.php
+++ index.php
@@ -1,16 +1,39 @@
 <?php
+	header("Access-Control-Allow-Origin:*");
+	header("Access-Control-Allow-Headers:*");
+	header("Content-type: application/json");
+
+	$json = null;
+	foreach($_POST as $post)
+		$json = $post;
 	
+	if($json!=null){
+		$json = json_decode($json,true);
+
+		$manager = new MongoDB\Driver\Manager($_ENV['URL_MONGODB']);
+		$query = new MongoDB\Driver\Query(['_id'=>'001']);
+		$rows = $manager->executeQuery("bot_translator.status",$query);
+
+		$response = null;
+		foreach ($rows as $row)
+			$response = $row;
 		
-	$manager = new MongoDB\Driver\Manager("mongodb+srv://bot:Hv11NXFH6zpf2h6M@pds.twdrk.mongodb.net/bot_translator?retryWrites=true&w=majority");
-	$query = new MongoDB\Driver\Query(['_id'=>'001']);
-	$rows = $manager->executeQuery("bot_translator.status",$query);
+		$response->running = $json["status"];
+		$response->now = $json["normal"];
+		$response->success_list = $json["suc_list"];
+		$response->fails_list = $json["fail_list"];
+		$response->count_success = $json["suc_count"];
+		$response->count_fails = $json["fail_count"];
 
-	$res = null;
-	foreach($rows as $row)
-	{
-		$res = $row;
+		$bulk = new MongoDB\Driver\BulkWrite;
+		$bulk->update(['_id'=>$response->_id],$response);
+
+		$res = $manager->executeBulkWrite("bot_translator.status",$bulk);
+
+		echo "Success";
+
 	}
-
-	echo $res->_id;
+	else
+		echo "error";
 		
 ?>
